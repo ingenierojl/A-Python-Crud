60834b36c41e5ab35fbabc4fc46417bc51405158
ingenierojl <ingenierorojas872@gmail.com>
2023-08-10 22:33:08 -0500

    funcionalidaddevideo

--- python/static/AppMain.py
+++ python/static/AppMain.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter #Importar modulo
 import uvicorn #importar
 from fastapi import Request, FastAPI, Form
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from funcion_insertar import *
 from funcion_select import *
+import cv2
+
+import numpy as np
+from pyzbar import pyzbar
 
 
 appr = FastAPI()
@@ -57,6 +61,44 @@ def homeusr(request: Request, eml: str):
     
     return templates.TemplateResponse("/html/usuarios.html", {"request": request, "datos": datos})
     #return templates.TemplateResponse("item.html", {"request": request, "id": id}
+def read_barcodes(frame):
+    barcodes = pyzbar.decode(frame)
+    for barcode in barcodes:
+        x, y , w, h = barcode.rect
+        #1
+        barcode_info = barcode.data.decode('utf-8')
+        cv2.rectangle(frame, (x, y),(x+w, y+h), (0, 255, 0), 2)
+        #2
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, barcode_info, (x + 6, y - 6), font, 2.0, (255, 255, 255), 1)
+        #3
+        with open("barcode_result.txt", mode ='w') as file:
+            file.write("Recognized Barcode:" + barcode_info)
+    return frame
+
+
+def generate_frames():
+    camera = cv2.VideoCapture(0)
+    while True:
+        ret, frame = camera.read()
+        if not ret:
+            break
+        frame = read_barcodes(frame)
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame_bytes = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+    camera.release()
+
+@appr.get("/video_feed")
+async def video_feed():
+    return StreamingResponse(generate_frames(), media_type="multipart/x-mixed-replace; boundary=frame")
+
+@appr.get("/obtenervideo")
+async def obtener_video(request: Request):
+    return templates.TemplateResponse("/html/obtener_video.html", {"request": request})
+
+
     
 if __name__=='__main__':
    
